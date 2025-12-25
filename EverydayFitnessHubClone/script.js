e9a5011db7e37db088ef856a8892afabc32dded8
Arslan Ali <aliarslan7770@gmail.com>
2025-12-25 12:01:10 +0500

    Featured product a nd Footer added

--- EverydayFitnessHubClone/script.js
+++ EverydayFitnessHubClone/script.js
@@ -1,2 +1,30 @@
 
+// let modeBtn = document.querySelector(".change-mode");
+// let currMode = "light";
+// let lastDiv = document.querySelector("body");
 
+// function change() {
+//     if (currMode === "light") {
+//         currMode = "dark";
+//         lastDiv.classList.remove("light");
+//         lastDiv.classList.add("dark");
+        
+//     }
+//     else {
+//         currMode = "light";
+//         lastDiv.classList.remove("dark");
+//         lastDiv.classList.add("light");
+//     }
+//     console.log(currMode);
+// }
+
+// modeBtn.addEventListener("click", change);
+
+
+
+
+
+
+
+
+ 
