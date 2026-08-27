957f5b15beb26c64ba9231e5a8629ca616a70cf5
Arslan Ali <aliarslan7770@gmail.com>
2026-08-27 10:37:57 +0500

    Make del button interactive

--- javascript-amazon-project-main/JS/checkout.js
+++ javascript-amazon-project-main/JS/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';  
 
@@ -17,7 +17,7 @@ cart.forEach(cartItem => {
 
 checkoutHTML +=`
 
-<div class="cart-item-container">
+<div class="cart-item-container js-item-${productId}">
     <div class="delivery-date">
       Delivery date:${new Date(new Date().setDate(new Date().getDate() + 7)).toLocaleDateString('en-US', options)}
 
@@ -41,7 +41,7 @@ checkoutHTML +=`
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary js-delete">
+          <span class="delete-quantity-link link-primary js-delete" data-product-id = "${matchingProductItem.id}">
             Delete
           </span>
         </div>
@@ -97,3 +97,14 @@ checkoutHTML +=`
     `
 });
 checkoutSummary.innerHTML = checkoutHTML;
+const delBtn = document.querySelectorAll(".js-delete");
+delBtn.forEach((link)=>{
+  link.addEventListener('click',()=>{
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+   const container = document.querySelector(`.js-item-${productId}`);
+   container.remove();
+
+  })
+ 
+});
--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,5 +1,5 @@
 
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
@@ -28,4 +28,17 @@ export function addToCart(productId) {
       quantity: quantitySelected
     });
   };
+}
+export function removeFromCart(productId){
+  let newCart =[];
+  cart.forEach(item=>{
+    if(item.productId!==productId){
+    newCart.push(item)
+  }
+  cart = newCart;
+
+  })
+  
+
+
 }
