46babc95f8a699c361fa68c8da5abdfd8bce1711
Arslan Ali <aliarslan7770@gmail.com>
2026-09-01 07:57:18 +0500

    create Order using backend

--- javascript-amazon-project-main/JS/checkout/paymentSummary.js
+++ javascript-amazon-project-main/JS/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import {cart, calculateCartQuantity} from "../../data/cart.js"
 import { getProduct } from "../../data/products.js";
 import { getdeliveryOptions } from "../../data/deliveryOption.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/order.js";
 
 export function renderPaymentSummary(){
   let productPriceCent=0;
@@ -47,7 +48,7 @@ export function renderPaymentSummary(){
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>`;
 
@@ -55,5 +56,25 @@ document.querySelector(".payment-summary").innerHTML = paymentSummaryHTML;
 let cartQuantity =0;
     cartQuantity = calculateCartQuantity();
     document.querySelector(".item-quantity").innerHTML = ` items (${cartQuantity})`;
+
+    document.querySelector(".js-place-order")
+    .addEventListener('click',async ()=>{
+      try{
+      const response = await fetch('https://supersimplebackend.dev/orders',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart,
+        })  
+      });
+      const order = await response.json();
+      addOrder(order);
+    } catch (error) {
+      console.error("Error placing order:", error);
+    }
+    window.location.href = 'orders.html';
+  });
 
 }
---
+++ javascript-amazon-project-main/data/order.js
@@ -0,0 +1,10 @@
+export const orders = localStorage.getItem('orders') ? JSON.parse(localStorage.getItem('orders')) : [];
+
+export function addOrder(order){
+  orders.unshift(order);
+  saveToStorage();
+}
+
+export function saveToStorage(){
+  localStorage.setItem('orders',JSON.stringify(orders));
+}
