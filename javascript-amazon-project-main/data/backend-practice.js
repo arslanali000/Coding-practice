e86af283ecdfc9d9a049b5e8ded7650a70bc67c6
Arslan Ali <aliarslan7770@gmail.com>
2026-09-01 06:40:50 +0500

    Backend and callbacks are used

--- javascript-amazon-project-main/data/backend-practice.js
+++ javascript-amazon-project-main/data/backend-practice.js
@@ -2,7 +2,7 @@ const xhr = new XMLHttpRequest();
 xhr.addEventListener('load',()=>{
   console.log(xhr.response);
 });
-xhr.open('GET', 'https://supersimplebackend.dev/');
+xhr.open('GET', 'https://supersimplebackend.dev/products');
 xhr.send();
 
 
